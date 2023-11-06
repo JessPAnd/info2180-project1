97395f53fa2a6ad32dd0c1948539817cda1decdc
Jessica Anderson <jespaiand@hotmail.com>
2023-11-05 19:01:01 -0500

    Added edited Starter files

--- scripts.js
+++ scripts.js
@@ -1,23 +1,25 @@
-const newsletterForm = document.querySelector('.newsletter form');
-
-/*event listener*/
-newsletterForm.addEventListener('submit', function(event) 
+document.addEventListener('DOMContentLoaded', (event) => 
 {
-    event.preventDefault();
-
-    // email input & message element
-    const emailInput = document.querySelector('.newsletter input[type="email"]');
-    const messageElement = document.querySelector('.newsletter .message');
-
-    // if the email input field is not empty
-    if (emailInput.value !== '') 
+    const newsletterForm = document.querySelector('.newsletter form');
+    /*event listener*/
+    newsletterForm.addEventListener('submit', function(event) 
     {
-        // Display a success message with the entered email address
-        messageElement.textContent = `Thank you! Your email address ${emailInput.value} has been added to our mailing list!`;
-    } 
-    else 
-    {
-        // Display an error message for empty email input
-        messageElement.textContent = 'Please enter a valid email address.';
-    }
+        event.preventDefault();
+
+        // email input & message element
+        const emailInput = document.querySelector('.newsletter input[type="email"]');
+        const messageElement = document.querySelector('.newsletter .message');
+
+        // if the email input field is not empty
+        if (emailInput.value !== '') 
+        {
+            // display a success message
+            messageElement.textContent = `Thank you! Your email address ${emailInput.value} has been added to our mailing list!`;
+        } 
+        else 
+        {
+            // display an error message 
+            messageElement.textContent = 'Please enter a valid email address.';
+        }
+    });
 });
